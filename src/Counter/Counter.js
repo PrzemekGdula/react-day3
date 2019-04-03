134479c6ca8f4a10bd7ac0f4d73c8047b764cdb8
PrzemekGdula <przemo.g12@wp.pl>
2019-04-03 10:49:43 +0200

    this state number +1

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -8,15 +8,21 @@ class Counter extends React.Component {
             number: 10,
         }
     }
-    
+
     render() {
+        const inc = () => this.state.number = this.state.number + 1
+            
+        
+
         return (
             <div>
                 <h1>
-                    0
+                    {this.state.number}
                 </h1>
 
-                <button>
+                <button
+                    onClick={inc}
+                >
                     +
                 </button>
             </div>
