14adf6662706d3c542b88b89737b5dd5aebcaba3
PrzemekGdula <przemo.g12@wp.pl>
2019-04-03 14:40:52 +0200

    router with links added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import Counter from './Counter'
 import TypeName from './TypeName'
 import Lifecycle from './Lifecycle';
@@ -6,7 +8,11 @@ import Lifecycle from './Lifecycle';
 
 const App = (props) => (
     <div>
-        <Lifecycle />
+        <Router>
+            <Link to="/">Home</Link>
+            <Link to="/Lifecycle">Lifecycle</Link>
+            <Route path={'/Lifecycle'} component={Lifecycle} />
+        </Router>
         <Counter number={5} />
         <Counter />
         <TypeName />
