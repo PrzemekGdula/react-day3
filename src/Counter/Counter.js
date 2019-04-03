1f263a4c5f99921a31b148f8c29a4f47ee26e677
PrzemekGdula <przemo.g12@wp.pl>
2019-04-03 10:40:47 +0200

    constructor with initial state

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,16 +1,24 @@
 import React from 'react'
 
 class Counter extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            number: 10,
+        }
+    }
+    
     render() {
         return (
             <div>
                 <h1>
                     0
-            </h1>
+                </h1>
 
                 <button>
                     +
-            </button>
+                </button>
             </div>
         )
     }
