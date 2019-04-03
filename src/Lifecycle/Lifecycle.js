c98731848cc781875f9a15a7f3af2a4ca5960df6
PrzemekGdula <przemo.g12@wp.pl>
2019-04-03 13:41:09 +0200

    componentDidMount added

--- src/Lifecycle/Lifecycle.js
+++ src/Lifecycle/Lifecycle.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 class Lifecycle extends React.Component {
+    constructor(){
+        super()
+
+        console.log('constructor')
+    }
+
+    componentDidMount () {
+        console.log('componentDidMount')
+    }
     render() {
+        console.log('render')
         return (
             <div>
                 Lifecycle
